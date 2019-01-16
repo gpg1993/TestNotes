631f46b418fccdacbd0af3466d2b2fdbb751fa90
gpg1993 <623808222@qq.com>
2019-01-16 20:40:13 +0800

    单例模式

---
+++ Singleton/HttpServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class HttpServer
+    {
+        /// <summary>
+        /// 服务器名称唯一
+        /// </summary>
+        public string ServerName { get; set; }
+        /// <summary>
+        /// 服务器处理容量
+        /// </summary>
+        public string ServerCapacity { get; set; }
+    }
+}
