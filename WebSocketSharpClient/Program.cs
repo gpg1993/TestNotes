d61d0a966f3abb4e03fde55402b8c1b830737ec5
gpg1993 <623808222@qq.com>
2019-01-21 18:01:32 +0800

    websocket

---
+++ WebSocketSharpClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace WebSocketSharpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebSocketRun();
+        }
+
+        public static async Task WebSocketRun()
+        {
+            var ws = new WebSocket("ws://dragonsnest.far/Laputa");
+            ws.OnMessage += Ws_OnMessage;
+            ws.OnError += Ws_OnError;
+            ws.OnClose += Ws_OnClose;
+            ws.Connect();
+            ws.Send("Hi,nihao!");
+            Console.ReadKey();
+            ws.Close();
+        }
+
+        private static void Ws_OnClose(object sender, CloseEventArgs e)
+        {
+            Console.WriteLine($"client:{e.Code}");
+        }
+
+        private static void Ws_OnError(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine($"client:{e.Message}");
+        }
+
+        private static void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.IsBinary)//判读数据类型e.IsText,e.IsPing
+            {
+
+            }
+            Console.WriteLine($"client:{e.Data}");
+        }
+    }
+}
---
+++ WebSocketSharpServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WebSocketSharpServer
+{
+    class Program
+    {
+        public class Laputa : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                var msg = e.Data == "BALUS"
+                          ? "I've been balused already..."
+                          : "I'm not available now.";
+
+                Send(msg);
+            }
+        }
+        static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer("ws://dragonsnest.far");
+            wssv.AddWebSocketService<Laputa>("/Laputa");
+            wssv.Start();
+            
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+}
