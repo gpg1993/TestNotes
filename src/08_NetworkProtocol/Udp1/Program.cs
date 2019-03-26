620412dd554480b166ef9b503f07fc948502d4af
gpg1993 <623808222@qq.com>
2019-03-26 11:24:03 +0800

    udp

---
+++ src/08_NetworkProtocol/Udp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Udp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(1000);
+            IPHostEntry iPHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress address = IPAddress.Parse("127.138.2.2");
+            IPEndPoint iPEndPoint = new IPEndPoint(address, 8080);
+            UdpClient udpClient = new UdpClient();
+            string temp = "您好";
+            int count = 20;
+            for (int i = 0; i < count; i++)
+            {
+                byte[] b = System.Text.Encoding.UTF8.GetBytes(temp + "---" + i);
+                udpClient.Send(b, b.Length, Dns.GetHostName(),8080);
+
+                byte[] c = System.Text.Encoding.UTF8.GetBytes(temp + "---hahaha");
+                udpClient.Send(c, c.Length, iPEndPoint);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/08_NetworkProtocol/Udp2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Udp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListenUdp1();
+            ListenUdp2();
+
+            Console.ReadKey();
+        }
+
+        public static void ListenUdp1()
+        {
+            IPAddress address1 = IPAddress.Parse("127.138.2.2");
+            IPEndPoint iep1 = new IPEndPoint(address1, 8080);
+            UdpClient uc = new UdpClient(iep1);
+            Action<UdpClient> UdpAction = (udp) => {
+                while (true)
+                {
+                    IPAddress[] addresses = Dns.GetHostAddresses(Dns.GetHostName());
+                    IPAddress address = IPAddress.Parse("192.168.2.1");
+                    IPEndPoint iep = new IPEndPoint(address, 8888);
+                    //获得Form1发送过来的数据包
+                    string text = System.Text.Encoding.UTF8.GetString(uc.Receive(ref iep));
+
+                    Console.WriteLine(iep.Address + "---" + iep.Port);
+                    Console.WriteLine(text);
+                }
+            };
+            Action action = () => { };
+            Task task1 = new Task(() => UdpAction(uc));
+            task1.Start();
+        }
+        public static void ListenUdp2()
+        {
+            UdpClient uc = new UdpClient(8080);
+            Action<UdpClient> UdpAction = (udp) => {
+                while (true)
+                {
+                    IPAddress[] addresses = Dns.GetHostAddresses(Dns.GetHostName());
+                    IPAddress address = IPAddress.Parse("192.168.2.1");
+                    IPEndPoint iep = new IPEndPoint(address, 8888);
+                    //获得Form1发送过来的数据包
+                    string text = System.Text.Encoding.UTF8.GetString(uc.Receive(ref iep));
+
+                    Console.WriteLine(iep.Address + "---" + iep.Port);
+                    Console.WriteLine(text);
+                }
+            };
+            Action action = () => { };
+            Task task1 = new Task(() => UdpAction(uc));
+            task1.Start();
+        }
+    }
+}
